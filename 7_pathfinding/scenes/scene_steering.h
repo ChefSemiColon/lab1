939ba502ee8cb1d842daed9e495c904f2984a401
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-04-11 00:26:05 +0100

    pathfinding done
    i done pathfginding

---
+++ 7_pathfinding/scenes/scene_steering.h
@@ -0,0 +1,13 @@
+//"scene_steering.h"
+
+#pragma once
+
+#include "engine.h"
+
+class SteeringScene : public Scene {
+public:
+  void Load() override;
+  void UnLoad() override;
+  void Update(const double& dt) override;
+  void Render() override;
+};
