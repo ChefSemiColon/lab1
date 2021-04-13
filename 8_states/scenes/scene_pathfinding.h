67597e87e573195b773f3fe489c0b22bdcece7ba
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-04-13 01:09:06 +0100

    statemachines dfopne
    gbhjm,

---
+++ 8_states/scenes/scene_pathfinding.h
@@ -0,0 +1,13 @@
+//"scene_pathfinding.h"
+#pragma once
+
+#include <engine.h>
+
+class PathfindingScene : public Scene
+{
+public:
+    void Load() override;
+    void UnLoad() override;
+    void Update(const double &dt) override;
+    void Render() override;
+};
